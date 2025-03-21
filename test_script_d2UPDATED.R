86abeafece918e3bb5f53004db42f04630af010f
JoeMarkWatson <joe.mark.watson@gmail.com>
2025-03-21 09:47:11 +0000

    21 Mar joe commit

--- test_script_d2UPDATED.R
+++ test_script_d2UPDATED.R
@@ -125,52 +125,53 @@ get_mean_values <- function(df) {
 }
 
 
-plot_lines = function(which_dfs=3) {
-  # Prepare the data by calculating mean distances from true theta for each dataframe
-  closed_means <- get_mean_values(closed_objs_r[[which_dfs]])
-  someNA_means <- get_mean_values(someNA_objs_r[[which_dfs]])
-  noNA_means <- get_mean_values(noNA_objs_r[[which_dfs]])  
-  bestSingle_means <- get_mean_values(bestSingle_objs_r[[which_dfs]])  
+plot_lines = function(obj_list, which_dfs = 3) {
   
-  DOevi_means <- get_mean_values(DOevi_objs_r[[which_dfs]])
-  DOcom_means <- get_mean_values(DOcom_objs_r[[which_dfs]])
-  DO_means <- get_mean_values(DO_objs_r[[which_dfs]])  
+  # Initialize an empty list to store processed data frames
+  processed_dfs <- list()
   
-  # Add a column to identify the type of CAT
-  closed_means$CAT <- "closed"
-  someNA_means$CAT <- "some NA"
-  noNA_means$CAT <- "no NA"
-  bestSingle_means$CAT <- "best single item" 
+  # Loop through each category in obj_list
+  for (cat_name in names(obj_list)) {
+    obj <- obj_list[[cat_name]]
+    df <- obj$data[[which_dfs]]
+    
+    # Only process if df is not NULL
+    if (!is.null(df)) {
+      df_means <- get_mean_values(df)
+      df_means$CAT <- obj$label
+      processed_dfs[[cat_name]] <- df_means  # Store processed data
+    }
+  }
   
-  DOevi_means$CAT <- "constant evidence only"
-  DOcom_means$CAT <- "constant comparison only"
-  DO_means$CAT <- "constant evi or compari" 
+  # Combine all processed data frames into one
+  combined_means <- bind_rows(processed_dfs)
   
-  combined_means <- bind_rows(closed_means, someNA_means, noNA_means, bestSingle_means, DOevi_means, DOcom_means, DO_means)
-  combined_means$item <- c(0:19)
+  # Ensure "item" column is added correctly
+  combined_means$item = 0:19
   rownames(combined_means) <- NULL
   
-  # Create the joined dot plot
-  if (which_dfs == 2 | which_dfs == 4) {
-    y_lab = ifelse(which_dfs==4, "Mean Distance from True Theta", "Mean Theta")
-    ggtitle = ifelse(which_dfs==4, "Connected Scatter Plot of Mean Distance from True Theta", "Connected Scatter Plot of Mean Theta")
-    print(ggplot(combined_means, aes(y = mean_value, x = item, color = CAT)) +
-            geom_point() + 
-            geom_line(aes(group = CAT), size = 1) +
-            theme_minimal() +
-            labs(x = "Closed Items Administered", y = y_lab, color = "Approach") +
-            ggtitle(ggtitle))
-  } else {
-    y_lab = "Mean Theta Est SE"
-    ggtitle = "Connected Scatter Plot of Mean Theta Est SE"
-    print(ggplot(combined_means, aes(y = mean_value, x = item, color = CAT)) +
-            geom_point() + 
-            geom_line(aes(group = CAT), size = 1) +
-            theme_minimal() +
-            labs(x = "Closed Items Administered", y = y_lab, color = "Approach") +
-            ggtitle(ggtitle) +
-            scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0.27, NA)))
+  # Define plot labels dynamically
+  y_lab <- ifelse(which_dfs == 5, "Mean Distance from True Theta (Bias)",
+                  ifelse(which_dfs == 4, "Mean Absolute Distance from True Theta", 
+                         ifelse(which_dfs == 2, "Mean Theta", "Mean Theta Est SE")))
+  ggtitle <- ifelse(which_dfs == 5, "Connected Scatter Plot of Mean Distance Between Theta Hat and True Theta (Bias)",
+                    ifelse(which_dfs == 4, "Connected Scatter Plot of Mean Absolute Distance Between Theta Hat and True Theta", 
+                           ifelse(which_dfs == 2, "Connected Scatter Plot of Mean Theta Hat", "Connected Scatter Plot of Mean Theta Hat Est SE")))
+  
+  # Generate plot
+  p <- ggplot(combined_means, aes(y = mean_value, x = item, color = CAT)) +
+    geom_point() + 
+    geom_line(aes(group = CAT), size = 1) +
+    theme_minimal() +
+    labs(x = "Closed Items Administered", y = y_lab, color = "Approach") +
+    ggtitle(ggtitle)
+  
+  # Adjust y-axis if which_dfs is not 2, 4 or 5
+  if (!(which_dfs %in% c(2, 4, 5))) {
+    p <- p + scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0.27, NA))
   }
+  
+  print(p)
 }
 
 
@@ -180,14 +181,17 @@ plot_lines = function(which_dfs=3) {
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # load some models
-load("output/fit_closed19022025_d2.RData")  # loads as fitc - the closed only baseline model
-load("output/fit_noNA19022025_d2.RData")  # loads as fit_no_NA
-load("output/fit_someNA19022025_d2.RData")  # loads as fit_some_NA
-load("output/fit_best_single19022025_d2.RData")  # loads as fit_best_single
-load("output/fit_DOevi19022025_d2.RData")  # loads as fit_DOevi
-load("output/fit_DOcom19022025_d2.RData")  # loads as fit_DOcom
-load("output/fit_DO19022025_d2.RData")  # loads as fit_DO
-#load("output/main_fit22122024_d2.RData")  # loads as fit  # old trial model
+load("output/fit_closed_d2_real.RData")  # loads as fitc - the closed only baseline model
+load("output/fit_bsi_d2_real.RData")
+load("output/fit_bciai_sNA_d2_real.RData")
+load("output/fit_bciai_nNA_d2_real.RData")
+load("output/fit_bcvc_sNA_d2_real.RData")
+load("output/fit_bcve_sNA_d2_real.RData")
+load("output/fit_bcve_nNA_d2_real.RData")
+load("output/fit_bccc_sNA_d2_real.RData")
+load("output/fit_bccc_nNA_d2_real.RData")
+load("output/fit_bcce_sNA_d2_real.RData")
+load("output/fit_bcce_nNA_d2_real.RData")
 
 # load GPT scores for essays and sents, as well as what items got selected
 file_paths <- list(
@@ -198,12 +202,16 @@ file_paths <- list(
   MarksGPT_DO_evidence = '../GPT_sentsOutputEvidence_01022025.csv',
   MarksGPT_DO_compare = '../GPT_sentsOutputCompare_01022025.csv',
   
-  items_to_keep_someNA = 'output/items_to_keep_someNA19022025.csv',
-  items_to_keep_noNA = 'output/items_to_keep_noNA19022025.csv',
-  items_to_keep_best_single = 'output/items_to_keep_best_single19022025.csv',
-  items_to_keepDOevi = 'output/items_to_keepDOevi19022025.csv',
-  items_to_keepDOcom = 'output/items_to_keepDOcom19022025.csv',
-  items_to_keepDO = 'output/items_to_keepDO19022025.csv'
+  items_to_keep_bsi = 'output/items_to_keep_bsi_REALd2.csv',
+  items_to_keep_bciai_sNA = 'output/items_to_keep_bciai_sNA_REALd2.csv',
+  items_to_keep_bciai_nNA = 'output/items_to_keep_bciai_nNA_REALd2.csv',
+  items_to_keep_bccc_sNA = 'output/items_to_keep_bccc_sNA_REALd2.csv',
+  items_to_keep_bccc_nNA = 'output/items_to_keep_bccc_nNA_REALd2.csv',
+  items_to_keep_bcce_sNA = 'output/items_to_keep_bcce_sNA_REALd2.csv',
+  items_to_keep_bcce_nNA = 'output/items_to_keep_bcce_nNA_REALd2.csv',
+  items_to_keep_bcvc_sNA = 'output/items_to_keep_bcvc_sNA_REALd2.csv',
+  items_to_keep_bcve_sNA = 'output/items_to_keep_bcve_sNA_REALd2.csv',
+  items_to_keep_bcve_nNA = 'output/items_to_keep_bcve_nNA_REALd2.csv'
 )
 datasets <- lapply(file_paths, read.csv)
 
@@ -238,34 +246,56 @@ data_all = merge(data_all, datasets$MarksGPT_DO_compare, by = 'ID')
 data_all[data_all == "False"] = F
 data_all[data_all == "True"] = T
 
-itk_sNA_df = load_kept_GPT_items(kept_items = datasets$items_to_keep_someNA, fit = fit_some_NA)
-itk_noNA_df = load_kept_GPT_items(kept_items = datasets$items_to_keep_noNA, fit = fit_no_NA)
-itkbs_df = load_kept_GPT_items(kept_items = datasets$items_to_keep_best_single, fit = fit_best_single)
-
-itk_DOevi_df = load_kept_GPT_items(kept_items = datasets$items_to_keepDOevi, fit = fit_some_NA)
-itk_DOcom_df = load_kept_GPT_items(kept_items = datasets$items_to_keepDOcom, fit = fit_no_NA)
-itk_DO_df = load_kept_GPT_items(kept_items = datasets$items_to_keepDO, fit = fit_best_single)
+# load item banks
+itk_bsi = load_kept_GPT_items(kept_items = datasets$items_to_keep_bsi, fit = fit_bsi)
+itk_bciai_sNA = load_kept_GPT_items(kept_items = datasets$items_to_keep_bciai_sNA, fit = fit_bciai_sNA)
+itk_bciai_nNA = load_kept_GPT_items(kept_items = datasets$items_to_keep_bciai_nNA, fit = fit_bciai_nNA)
+itk_bccc_sNA = load_kept_GPT_items(kept_items = datasets$items_to_keep_bccc_sNA, fit = fit_bccc_sNA)
+itk_bccc_nNA = load_kept_GPT_items(kept_items = datasets$items_to_keep_bccc_nNA, fit = fit_bccc_nNA)
+itk_bcce_sNA = load_kept_GPT_items(kept_items = datasets$items_to_keep_bcce_sNA, fit = fit_bcce_sNA)
+itk_bcce_nNA = load_kept_GPT_items(kept_items = datasets$items_to_keep_bcce_nNA, fit = fit_bcce_nNA)
+itk_bcvc_sNA = load_kept_GPT_items(kept_items = datasets$items_to_keep_bcvc_sNA, fit = fit_bcvc_sNA)
+itk_bcve_sNA = load_kept_GPT_items(kept_items = datasets$items_to_keep_bcve_sNA, fit = fit_bcve_sNA)
+itk_bcve_nNA = load_kept_GPT_items(kept_items = datasets$items_to_keep_bcve_nNA, fit = fit_bcve_nNA)
 
 closed_only_df = data_all[closed_items]
 
 # run the sim
 closed_objs_r = cat_sim(fit_obj = fitc, data_all = closed_only_df)
-someNA_objs_r = cat_sim(fit_obj = fit_some_NA, data_all = itk_sNA_df)
-noNA_objs_r = cat_sim(fit_obj = fit_no_NA, data_all = itk_noNA_df)
+bsi_objs_r = cat_sim(fit_obj = fit_bsi, data_all = itk_bsi)
+bciai_sNA_objs_r = cat_sim(fit_obj = fit_bciai_sNA, data_all = itk_bciai_sNA)
+bciai_nNA_objs_r = cat_sim(fit_obj = fit_bciai_nNA, data_all = itk_bciai_nNA)
+bccc_sNA_objs_r = cat_sim(fit_obj = fit_bccc_sNA, data_all = itk_bccc_sNA)
+bccc_nNA_objs_r = cat_sim(fit_obj = fit_bccc_nNA, data_all = itk_bccc_nNA)
+bcce_sNA_objs_r = cat_sim(fit_obj = fit_bcce_sNA, data_all = itk_bcce_sNA)
+bcce_nNA_objs_r = cat_sim(fit_obj = fit_bcce_nNA, data_all = itk_bcce_nNA)
+bcvc_sNA_objs_r = cat_sim(fit_obj = fit_bcvc_sNA, data_all = itk_bcvc_sNA)
+bcve_sNA_objs_r = cat_sim(fit_obj = fit_bcve_sNA, data_all = itk_bcve_sNA)
+bcve_nNA_objs_r = cat_sim(fit_obj = fit_bcve_nNA, data_all = itk_bcve_nNA)
 
-DOevi_objs_r = cat_sim(fit_obj = fit_DOevi, data_all = itk_DOevi_df)
-DOcom_objs_r = cat_sim(fit_obj = fit_DOcom, data_all = itk_DOcom_df)
-DO_objs_r = cat_sim(fit_obj = fit_DO, data_all = itk_DO_df)
+# check sim output
+obj_list <- list(
+  closed = list(data = closed_objs_r, label = "closed"),
+  bsi = list(data = bsi_objs_r, label = 'best single item'),
+  bciai_sNA = list(data = bciai_sNA_objs_r, label = "best all items"),
+  bciai_nNA = list(data = bciai_nNA_objs_r, label = "best all items, no NA permitted"),
+  bccc_sNA = list(data = bccc_sNA_objs_r, label = "consistent comparison only"),
+  bccc_nNA = list(data = bccc_nNA_objs_r, label = "consistent comparison only, no NA permitted"),
+  bcce_sNA = list(data = bcce_sNA_objs_r, label = "consistent evidence only"),    
+  bcce_nNA = list(data = bcce_nNA_objs_r, label = "consistent evidence only, no NA permitted"),    
+  bcvc_sNA = list(data = bcvc_sNA_objs_r, label = "varying comparison only"),    
+  bcve_sNA = list(data = bcve_sNA_objs_r, label = "varying evidence only"),
+  bcve_nNA = list(data = bcve_nNA_objs_r, label = "varying evidence only, no NA permitted")
+)
 
-bestSingle_objs_r = cat_sim(fit_obj = fit_best_single, data_all = itkbs_df)
 
 # plot
-plot_lines(which_dfs = 3)  # warning message is OK - it is for missing 0th item value for closed only
-plot_lines(which_dfs = 2)  # shows that there is overall shift in est theta - to be revealed through sim whether this is OK
+plot_lines(obj_list, which_dfs = 3)  # warning message is OK - it is for missing 0th item value for closed only
+plot_lines(obj_list, which_dfs = 2)  # shows that there is overall shift in est theta - to be revealed through sim whether this is OK
 
 # look at some of the individual points on the plot
 mean(closed_objs_r[[3]]$X2)
-mean(someNA_objs_r[[3]]$X2)
+mean(bciai_sNA_objs_r[[3]]$X2)
 mean(closed_objs_r[[3]]$X20)
-mean(someNA_objs_r[[3]]$X20)
+mean(bciai_sNA_objs_r[[3]]$X20)
 
